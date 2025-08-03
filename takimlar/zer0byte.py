535b5ad2690bddb9196f3f410efafd7d997cea78
GtxAnqel <qrwr34936@gmail.com>
2025-08-04 00:46:41 +0300

    Create zer0byte.py

---
+++ takimlar/zer0byte.py
@@ -0,0 +1,15 @@
+from sistem.sahne_yazici import sahne
+
+def saldir():
+    sahne([
+        "🔨 Zer0Byte saldırıya geçti!",
+        "📡 Sistem açıklarını tarıyor...",
+        "🦠 Exploit modülü devreye alındı!",
+    ], renk='magenta')
+
+def savun():
+    sahne([
+        "🧱 Zer0Byte savunmada!",
+        "🔒 Kritik veriler şifreleniyor...",
+        "🧬 Karşı saldırı için yapay zeka aktif!",
+    ], renk='blue')
